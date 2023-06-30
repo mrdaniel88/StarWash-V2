b4d788c116cb71cbaa5b223ce8add5895ec90000
Daniel Martinez <d.martinez74@gmail.com>
2023-06-30 22:52:41 +0000

    home made responsive

--- src/front/js/pages/aboutUs.js
+++ src/front/js/pages/aboutUs.js
@@ -34,43 +34,43 @@ export const AboutUs = () => {
                     <div className="col-sm-8 min-vh-100">
 
                         <div className="d-flex">
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://plus.unsplash.com/premium_photo-1664475605026-e1feb5c77922?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80" className="card-img-top" alt="..." />
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://plus.unsplash.com/premium_photo-1664298082231-d3dacfe475d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://plus.unsplash.com/premium_photo-1661684449481-c5abe43a82c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
                             </div>
                         </div>
 
 
                         <div className="d-flex">
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://images.unsplash.com/photo-1518830892177-d7611245a832?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
 
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://images.unsplash.com/photo-1622426141446-5148b6fa1115?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
 
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://images.unsplash.com/photo-1678986489261-6cf27bd58175?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
 
                             </div>
                         </div>
 
                         <div className="d-flex">
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://plus.unsplash.com/premium_photo-1661778990198-076b9656b54d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80" className="card-img-top" alt="..." />
 
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://images.unsplash.com/photo-1465172018141-c4e4fa823289?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="card-img-top" alt="..." />
 
                             </div>
-                            <div className="card border-0 custom-aboutUs" style={{ width: "18rem", padding: '5px' }}>
+                            <div className="card border-0 custom-aboutUs w-100" style={{ width: "18rem", padding: '5px' }}>
                                 <img src="https://images.unsplash.com/photo-1485456780483-06106d602fef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80" className="card-img-top" alt="..." />
 
                             </div>

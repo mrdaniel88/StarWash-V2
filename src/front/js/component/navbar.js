a49bfdf322b28f25ac06e273ff2400291015b9dc
Daniel Martinez <d.martinez74@gmail.com>
2023-06-30 21:39:01 +0000

    navbar with star wars colors

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -20,52 +20,46 @@ export const Navbar = () => {
 		}
 	}
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light custom-navbar">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-navbar">
 			<div className="container-fluid">
 				<Link to="/" className="text-decoration-none">
-					<h1 className="text-black" style={{ fontSize: '36px' }}>StarWash</h1>
+					<h1 className="page-title">StarWash</h1>
 				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse justify-content-center p-2" id="navbarNav">
 					<ul className="navbar-nav ">
-						<li className="nav-item px-3">
+						<li className="nav-item px-3 nav-element">
 							<Link to="/" className="nav-link active" aria-current="page">Home</Link>
 						</li>
 						<li className="nav-item px-3">
-							<Link to="/aboutUs" className="nav-link active" aria-current="page">About Us</Link>
+							<Link to="/aboutUs" className="nav-link active nav-element" aria-current="page">About Us</Link>
 						</li>
 						<li className="nav-item px-3">
-							<Link to="/services" className="nav-link active" aria-current="page" >Services</Link>
+							<Link to="/services" className="nav-link active nav-element" aria-current="page" >Services</Link>
 						</li>
 						<li className="nav-item px-3">
-							<Link to="/frecuently-asked-questions" className="nav-link active" aria-current="page">Frequently Asked Questions</Link>
+							<Link to="/frecuently-asked-questions" className="nav-link active nav-element" aria-current="page">Frequently Asked Questions</Link>
 						</li>
 
 					</ul>
 				</div>
 				<div className="nav-item justify-content-end">
 					<div className="btn-group" role="group" aria-label="Basic example">
-						
 						{
 							!!store.accessToken ?
-								<><button onClick={logout} type="button" className="custom-button rounded" style={{ marginRight: '10px', backgroundColor: '#E9C46A', color: '#000', padding: '18px 32px', borderRadius: '6px', boxShadow: '0 6px 0 black' }}>Logout</button><Link to="/shoppingCar">
-
-
-
-									<button type="button" className="custom-button rounded position-relative" style={{ marginRight: '10px', backgroundColor: '#E9C46A', color: '#000', padding: '18px 32px', borderRadius: '6px', boxShadow: '0 6px 0 black' }}>
+								<><button onClick={logout} type="button" className="nav-button custom-button">Logout</button><Link to="/shoppingCar">
+									<button type="button" className="nav-button custom-button position-relative">
 										<i className="fa-solid fa-cart-shopping"></i>
-
 										<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
 											{store.user_services.length}
-
 										</span>
 									</button>
 								</Link></>
 								:
-								<><Link to="/Login"><button type="button" className="custom-button rounded" style={{ marginRight: '10px', backgroundColor: '#E9C46A', color: '#000', padding: '18px 32px', borderRadius: '6px', boxShadow: '0 6px 0 black' }}>Login</button></Link>
-									<Link to="/Register"><button type="button" className="custom-button rounded" style={{ marginRight: '10px', backgroundColor: '#E9C46A', color: '#000', padding: '18px 32px', borderRadius: '6px', boxShadow: '0 6px 0 black' }}>SignUp</button></Link></>
+								<><Link to="/Login"><button type="button" className="nav-button custom-button">Login</button></Link>
+									<Link to="/Register"><button type="button" className="nav-button custom-button">SignUp</button></Link></>
 						}
 
 					</div>
